15a5d91bc96ca298a696706fdb7edbaa4a444f30
maitrecanard <mathieusiaudeau@gmail.com>
2022-10-21 09:08:59 +0200

    modification du formulaire

--- src/Form/ProductsType.php
+++ src/Form/ProductsType.php
@@ -15,6 +15,8 @@ use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\File;
+
 
 class ProductsType extends AbstractType
 {
@@ -24,16 +26,13 @@ class ProductsType extends AbstractType
             ->add('name', TextType::class, [
                 'label' => 'Nom',
             ])
-            ->add('subtitle')
+            ->add('subtitle', TextType::class, [
+                'label' => 'Sous titre',
+            ])
             ->add('description', TextareaType::class, [
                 'label' => 'Description'
             ])
-            /*->add('picture', FileType::class, [
-                'label' => 'Image du produit',
-                [
-                    'data_class' => null
-                ]
-            ])*/
+            
             ->add('categorie', EntityType::class, [
                 'class' => Categories::class,
                 'choice_label' => 'name',
@@ -47,6 +46,32 @@ class ProductsType extends AbstractType
             ])
             ->add('content', TextareaType::class, [
                 'label' => 'Contenu'
+            ])
+            ->add('picture', FileType::class, [
+                'label' => 'Image du produit',
+                'attr' => [
+                   'class' => 'form-controll'
+                ],
+                'row_attr' => [
+                    'class' => 'form-control',
+                    'value' => 'télécharger une image'
+                ],
+               
+                'mapped' => false,
+                'constraints' => [
+                    new File([
+                        'maxSize' => '42048k',
+                        'mimeTypes' => [
+                            'image/jpeg',
+                            'image/jpg',
+                            'image/png',
+                            'image/bmp',
+                            'image/gif'
+                        ],
+                        'mimeTypesMessage' => 'Merci d\'uploader uniquement une image au format PNG, JPG, BMP ou GIF !',
+                    ])
+                ],
+
             ])
             ->add('active', ChoiceType::class, [
                 'label' => 'Publié',
