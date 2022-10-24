ebcb43bc7155dddebbd5f257610b4a524bc43e94
maitrecanard <mathieusiaudeau@gmail.com>
2022-10-24 06:57:45 +0200

    start prog fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,12 +4,26 @@ namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use App\Entity\User;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        
+        $users = [];
+        $usersList = $this->user();
+
+        foreach($usersList AS $u) {
+            $user = new User();
+            $user->setEmail($u['email']);
+            $user->setFirstname($u['firstname']);
+            $user->setLastname($u['lastname']);
+            $user->setRoles([$u['roles']]);
+            $user->setPassword('Abcd1234!');
+
+            $manager->persist($user);
+            $usersList[] = $user;
+        }
 
         $manager->flush();
     }
@@ -21,15 +35,13 @@ class AppFixtures extends Fixture
                 "email" => "mathieusiaudeau@gmail.com",
                 "firstname" => "Mathieu",
                 "lastname" => "Siaudeau",
-                "roles" => "ROLE_SUPADMIN",
-                "password" => "Abcd1234!"
+                "roles" => "ROLE_SUPADMIN"
             ],
             2 => [
                 "email" => "contact@brasseriedifre.fr",
                 "firstname" => "Brasserie",
                 "lastname" => "Difré",
-                "roles" => "ROLE_ADMIN",
-                "password" => "Abcd1234!"
+                "roles" => "ROLE_ADMIN"
             ]
         ];
 
