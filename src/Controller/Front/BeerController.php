05fd984ed5ed7318775a01e07bfbe323f7e98f18
maitrecanard <mathieusiaudeau@gmail.com>
2022-12-09 16:06:17 +0100

    mise en place controller beer

---
+++ src/Controller/Front/BeerController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller\Front;
+
+use App\Repository\ProductsRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class BeerController extends AbstractController
+{
+    #[Route('/bieres', name: 'app_front_beer')]
+    public function index(ProductsRepository $productsRepository): Response
+    {
+
+        return $this->render('front/beer/index.html.twig', [
+            'products' => $productsRepository->findBy(['active'=>1, 'period'=> NULL], ['id' => 'DESC']),
+            'productsPeriod' => $productsRepository->findBy(['active'=>1, 'period'=> !NULL], ['id' => 'DESC']),
+            'goodies' => $productsRepository->findBy(['active'=>1, 'period'=> !NULL, 'degre'=> !NULL], ['id' => 'DESC']),
+        ]);
+    }
+
+   // #[Route('/b')]
+}
